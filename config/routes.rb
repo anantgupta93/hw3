f702711a8623a00fd02faf1be3a1e3cca0e5a66f
anantgupta93 <anant.gupta@kellogg.northwestern.edu>
2024-02-19 01:57:57 +0000

    hw3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+resources "places"
+resources "entries"
+#get "/places", :controller=> "place", :action => "index" 
+
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
 end
+
